9c5066c939d6092a4a1888b628de60995b01591c
leolee9086 <464803712@qq.com>
2022-05-14 23:19:37 +0800

    baselib更名

---
+++ baselib/components/cc-loader-file.vue
@@ -0,0 +1,28 @@
+<template>
+    <el-button @click="加载句柄()" icon="el-icon-folder" size="mini"></el-button>
+</template>
+<script>
+    module.exports={
+        props:["type"],
+        name:"cc-loader-file",
+        data(){
+            return {
+                fileHandle:{},
+                directoryHandle:{}
+            }
+        },
+        methods: {
+            加载句柄:function(){
+                this.type=="file"?this.加载文件():this.加载文件夹()
+            },
+            加载文件:async function(){
+                this.fileHandle=await window.showOpenFilePicker()
+                this.$emit("filechange",this.fileHandle)
+            },
+            加载文件夹:async function(){
+                this.directoryHandle = await window.showDirectoryPicker()
+                this.$emit("filechange",this.directoryHandle)
+            }
+        },
+    }
+</script>
